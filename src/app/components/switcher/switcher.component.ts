14e6c9fdff0e9b751b9ccdbe6ca4bac273db6670
ClaireAmor <claireamor7033@gmail.com>
2021-10-27 09:14:57 +0200

    7 assignement des color

--- src/app/components/switcher/switcher.component.ts
+++ src/app/components/switcher/switcher.component.ts
@@ -1,4 +1,5 @@
-import {Component} from '@angular/core';
+import {Component, Inject} from '@angular/core';
+import {DOCUMENT} from "@angular/common";
 
 @Component({
   selector: 'switcher',
@@ -7,19 +8,29 @@ import {Component} from '@angular/core';
 })
 
 export class SwitcherComponent {
+  //Injection du document afin qu'il ne soit jamais nul
+  constructor(@Inject(DOCUMENT) private readonly document: Document) {}
 
   isVisible = false;
+  themeList=['default','pink','yellow','green','purple','light-blue'];
+
+
 
   public windowScroll() :void {
+    /*Ici on créer un composant afin que si le document peut etre nul
+    et qu'il est impossible d'utiliser le @ts-ignore le document ne pense pas
+    qu'il puisse etre nul --> constructeur*/
+    const html = document.getElementById("back-to-top");
+    if (!html) return
+
     if (
       document.body.scrollTop > 100 ||
       document.documentElement.scrollTop > 100
     ) {
-      // @ts-ignore
-      document.getElementById("back-to-top").style.display = "inline";
+      /* // @ts-ignore */
+      html.style.display = "inline";
     } else {
-      // @ts-ignore
-      document.getElementById("back-to-top").style.display = "none";
+      html.style.display = "none";
     }
   }
 
